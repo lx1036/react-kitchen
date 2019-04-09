7e084565c67c39efdc3d90302428715c9f247c27
Xiang Liu <xiang.liu@rightcapital.com>
2019-04-09 21:59:27 +0800

    Add antd

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Admin} from "./Components/Admin";
 
 class App extends Component {
   render() {
---
+++ src/Components/Admin.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export class Admin extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header></Header>
+      </div>
+    );
+  }
+}
